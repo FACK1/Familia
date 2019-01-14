0548bfebadf77e16d043d4e01a122ff310cf02d9
fdo2 <fdo2@live.com>
2019-01-14 11:33:44 +0200

    set eslint

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-  res.render("home");
-})
+router.get('/', (req, res) => {
+  res.render('home');
+});
 
-module.exports = router
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const app = require("./app.js");
+const app = require('./app.js');
 
-app.set("PORT", 6002)
-app.listen(app.get("PORT"), ()=>{
-  console.log(`The Server Is Up And Running On http://localhost:6002`);
-})
+app.set('PORT', 6002);
+app.listen(app.get('PORT'), () => {
+  // eslint-disable-next-line no-console
+  console.log('The Server Is Up And Running On http://localhost:6002');
+});
