4fe74317e0964d5bac5f62569ee110d292143b13
fdo2 <fdo2@live.com>
2019-01-14 10:03:43 +0200

    set and configure handlebars #13

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 app.get("/", (req, res)=>{
-  res.send("Hello express!")
+  res.send("hello express!");
 })
 
+app.get("/home", (req, res)=>{
+  res.render("home");
+})
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    defaultLayout: "main"
+  })
+);
 
 module.exports = app
