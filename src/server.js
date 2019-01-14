2fa0fe17a88cab5150784cd9699c262665298f25
fdo2 <fdo2@live.com>
2019-01-14 11:56:03 +0200

    fixing heroku's deployment bugs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require('./app.js');
+require("env2")("config.env");
 
-app.set('PORT', 6002);
+app.set("PORT", process.env.PORT);
 app.listen(app.get('PORT'), () => {
   // eslint-disable-next-line no-console
   console.log('The Server Is Up And Running On http://localhost:6002');
