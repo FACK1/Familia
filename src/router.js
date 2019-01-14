d478d61e439b72108ca76c5e299587f13b7f644b
fdo2 <fdo2@live.com>
2019-01-14 10:53:25 +0200

    changes requested by scrum master

--- src/router.js
+++ src/router.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-  res.send("hello express!");
-})
-
-router.get("/home", (req, res)=>{
   res.render("home");
 })
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
-const express = require('express');
 const app = require("./app.js");
 
 app.set("PORT", 6002)
 app.listen(app.get("PORT"), ()=>{
-  console.log(`The Server Is Up And Running`);
+  console.log(`The Server Is Up And Running On http://localhost:6002`);
 })
