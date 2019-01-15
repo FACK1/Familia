d5c705f4fbba852ed86b208c36c3a40b6dc87476
orjwan98 <orjwan98@hotmail.com>
2019-01-15 13:26:03 +0200

    add register router #3

--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const userController = require("./controllers/userController");
 
-router.get('/', (req, res) => {
-  res.render('home');
+router.get("/", (req, res) => {
+  res.render("home");
 });
+
+router.post("/register", userController.register);
 
 module.exports = router;
