9825831d236728d41ad016c0af27444f169dc64b
orjwan98 <orjwan98@hotmail.com>
2019-01-16 10:28:32 +0200

    add GET /auth router #3

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("./controllers/userController");
+const express = require('express');
 
-router.get("/", (req, res) => {
-  res.render("home");
+const router = express.Router();
+const authController = require('./controllers/authController');
+
+router.get('/', (req, res) => {
+  res.render('home');
 });
 
-router.post("/register", userController.register);
+router.post('/register', authController.register);
+router.get('/auth', authController.index);
 
 module.exports = router;
