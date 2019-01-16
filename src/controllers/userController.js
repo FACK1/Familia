77c01be18778f9fd32cd0785d3ae0e4a76630228
orjwan98 <orjwan98@hotmail.com>
2019-01-16 09:41:55 +0200

    create /register route #3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,17 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User.model.js');
 
-const generateSalt = password => new Promise((resolve, reject) => {
+const checkUser = user => new Promise((resolve, reject) => {
+  User.findOne({ username: user.username }).then((foundUser) => {
+    if (foundUser) {
+      reject('already registered');
+    } else {
+      resolve(user);
+    }
+  });
+});
+
+const generateSalt = () => new Promise((resolve, reject) => {
   bcrypt.genSalt(10, (saltErr, salt) => {
     if (saltErr) {
       reject(saltErr);
@@ -10,37 +20,41 @@ const generateSalt = password => new Promise((resolve, reject) => {
     }
   });
 });
-const checkUser = username => new Promise((resolve, reject) => {
-  User.findOne({ username }).then((user) => {
-    if (user) {
-      reject('already registered');
-    } else {
-      resolve();
-    }
+
+const hashPassword = user => new Promise((resolve, reject) => {
+  generateSalt().then((salt) => {
+    bcrypt.hash(user.password, salt, (hashErr, hashedPassword) => {
+      if (hashErr) {
+        reject(hashErr);
+      } else {
+        user.password = hashedPassword;
+        resolve(user);
+      }
+    });
   });
 });
+
 const register = (req, res) => {
   console.log('HERE!!!', req.body);
   const { name, username, password } = req.body;
+  const u = new User({
+    name,
+    username,
+    password,
+  });
+  checkUser(u)
+    .then(hashPassword)
+    .then((acceptedUser) => {
+      acceptedUser.save((err) => {
+        if (err) {
+          res.send('There was a problem with registartion, please try again');
+        } else {
+          res.send('success!');
+        }
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
 };
-
 module.exports = { register };
-
-bcrypt.hash(password, salt, (hashErr, hashedPassword) => {
-  if (hashErr) {
-    res.send('Error registering');
-  } else {
-    const u = new User({
-      name,
-      username,
-      password: hashedPassword,
-    });
-    u.save((err) => {
-      if (err) {
-        res.send('There was a problem with registartion, please try again');
-      } else {
-        res.send('success!');
-      }
-    });
-  }
-});
