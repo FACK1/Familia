620a6e5a79da0aff1fa1d75a3968e25601d275fd
fdo2 <fdo2@live.com>
2019-01-16 20:35:31 +0200

    set login.js code#2

---
+++ public/js/login.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const loginUsername = document.getElementById('loginUsername').value;
+  const loginPassword = document.getElementById('loginPassword').value;
+
+  const userData = {
+    username: loginUsername,
+    password: loginPassword,
+  };
+
+  const url = '/login';
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(userData),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => {
+      if (res.redirected) {
+        window.location.href = res.url;
+      } else {
+        throw Error;
+      }
+    })
+    .catch((err) => {
+      alert('there was an error, try again');
+    });
+});
