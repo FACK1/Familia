fb26aeaf3f1877c5520c4a94850e8b637a5d57c7
mhmdtshref <mhmdtshref@gmail.com>
2019-01-16 15:00:34 +0200

    update code to be simpler #3

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,68 +1,36 @@
 const bcrypt = require('bcryptjs');
+//const cookie = require('cookie');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User.model.js');
+require('env2')('config.env');
 
 const index = (req, res) => {
   res.render('auth');
 };
 
-const checkUser = user => new Promise((resolve, reject) => {
-  User.findOne({ username: user.username }).then((foundUser) => {
-    if (foundUser) {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject('already registered');
-    } else {
-      resolve(user);
-    }
-  });
-});
-
-const generateSalt = () => new Promise((resolve, reject) => {
-  bcrypt.genSalt(10, (saltErr, salt) => {
-    if (saltErr) {
-      reject(saltErr);
-    } else {
-      resolve(salt);
-    }
-  });
-});
-
-const hashPassword = user => new Promise((resolve, reject) => {
-  generateSalt().then((salt) => {
-    bcrypt.hash(user.password, salt, (hashErr, hashedPassword) => {
-      if (hashErr) {
-        reject(hashErr);
-      } else {
-        const u = new User({
-          name: user.name,
-          username: user.username,
-          password: hashedPassword,
-        });
-        resolve(u);
-      }
-    });
-  });
-});
 
 const register = (req, res) => {
   const { name, username, password } = req.body;
-  const u = new User({
-    name,
-    username,
-    password,
-  });
-  checkUser(u)
-    .then(hashPassword)
-    .then((acceptedUser) => {
-      acceptedUser.save((err) => {
+  bcrypt.hash(password, 10)
+    .then(hashedPassword => new User({
+      name,
+      username,
+      password: hashedPassword,
+    })).then(user => new Promise((resolve, reject) => {
+      user.save((err, user) => {
         if (err) {
-          res.send('There was a problem with registartion, please try again');
+          reject(err);
         } else {
-          res.redirect('/');
+          resolve(user.id);
         }
       });
+    }))
+    .then(() => {
+        res.redirect('/');
     })
     .catch((err) => {
       res.send(err);
     });
 };
+
 module.exports = { register, index };
