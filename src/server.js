190bdca0937aedfda6f3526bc8b1c1ea8db4e0be
mhmdtshref <mhmdtshref@gmail.com>
2019-01-20 15:38:50 +0200

    run the server before the mongoose database connection #13

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const app = require('./app.js');
 require('env2')('config.env');
-require('./dbConfig/mongodb.config');
 
 app.set('PORT', process.env.PORT);
 app.listen(app.get('PORT'), () => {
   // eslint-disable-next-line no-console
   console.log(`The Server Is Up And Running On http://localhost:${app.get('PORT')}`);
+  require('./dbConfig/mongodb.config');
 });
