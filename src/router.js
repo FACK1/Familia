2fe9f4f7b4143b9e9ccf54fe37a1b1d866844827
mhmdtshref <mhmdtshref@gmail.com>
2019-01-20 15:37:51 +0200

    add joinFamily routers #5

--- src/router.js
+++ src/router.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const middlewares = require('./middlewares');
-
 const router = express.Router();
-const authController = require('./controllers/authController');
 
-router.get('/', [middlewares.checkAuth, middlewares.chechFamily], (req, res) => {
+const authController = require('./controllers/authController');
+const familyController = require('./controllers/familyController');
+
+router.get('/', [middlewares.checkAuth, middlewares.checkFamily], (req, res) => {
   res.render('home');
 });
 
 router.get('/joinFamily', (req, res) => {
   res.send('Join Family Page!');
 });
+
+router.post('/createFamily', [middlewares.checkAuth] , familyController.createFamily);
 
 router.post('/register', authController.register);
 router.get('/auth', authController.index);
