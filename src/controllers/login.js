3c8eb6b3bd9fcf3ccc5ad07d9ae02d636583443a
orjwan98 <orjwan98@hotmail.com>
2019-01-20 16:03:05 +0200

    commented some code

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,30 +1,25 @@
-const User = require('../models/User.model.js');
-const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-require('env2')('./config.env');
-const { SECRET } = process.env
-
-
-const checkUser = user => new Promise((resolve, reject) => {
-  User.findOne({ username: user.username }).then((foundUser) => {
-    if (!foundUser) {
-      reject('username does not exist');
-    } else {
-      resolve(user);
-    }
-  });
-})
-const checkPassword = user => new Promise((resolve, reject) => {
-  User.findOne({ username: user.username }).then((foundUser) => {
-    if (!foundUser) {
-      reject('username does not exist');
-    } else {
-      resolve(user);
-    }
-  });
-})
-
-const login = (req, res) =>{
-  {username, password} = req.body
-
-}
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/User.model.js');
+// require('env2')('./config.env');
+//
+// const { SECRET } = process.env;
+//
+// const checkUser = user => new Promise((resolve, reject) => {
+//   User.findOne({ username: user.username }).then((foundUser) => {
+//     if (!foundUser) {
+//       reject('username does not exist');
+//     } else {
+//       resolve(user);
+//     }
+//   });
+// });
+// const checkPassword = user => new Promise((resolve, reject) => {
+//   User.findOne({ username: user.username }).then((foundUser) => {
+//     if (!foundUser) {
+//       reject('username does not exist');
+//     } else {
+//       resolve(user);
+//     }
+//   });
+// });
