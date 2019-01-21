af7322ccd2d1d5971260081f0fe566185e4f4266
mhmdtshref <mhmdtshref@gmail.com>
2019-01-21 16:01:18 +0200

    rename and add addItem function #6

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -13,6 +13,7 @@ const getUserById = userId => new Promise((resolve, reject) => {
     });
 });
 
+
 const getItemsByFamilyId = familyId => new Promise((resolve, reject) => {
   Item.find({
     family_id: familyId,
@@ -39,6 +40,41 @@ const index = (req, res) => {
 };
 
 
+const createItem = (req, res) => {
+  (new Promise((resolve, reject) => {
+    const { itemTitle } = req.body;
+    User.findById(req.userId)
+      .exec((err, user) => {
+        if (err) {
+          reject(err);
+        } else {
+          const item = new Item({
+            title: itemTitle,
+            status: false,
+            family_id: user.family_id,
+          });
+          resolve(item);
+        }
+      });
+  }))
+    .then(item => new Promise((resolve, reject) => {
+      item.save((err, savedItem) => {
+        if (!savedItem) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    }))
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(() => {
+      res.redirect('/');
+    });
+};
+
 module.exports = {
   index,
+  createItem,
 };
