9f185992718d3af1efd435c6b9ab44849e29dddc
mhmdtshref <mhmdtshref@gmail.com>
2019-01-21 09:04:31 +0200

    tidy-up familyController #5

--- src/controllers/familyController.js
+++ src/controllers/familyController.js
@@ -1,28 +1,53 @@
-const Family = require('../models/Family.model');
-cosnt User  = require('../models/User.model');
 const randomize = require('randomatic');
+const Family = require('../models/Family.model');
+const User = require('../models/User.model');
+
+
+const checkSavedFamily = isSavedFamily => new Promise((resolve, reject) => {
+  if (!isSavedFamily) {
+    reject();
+  } else {
+    resolve(isSavedFamily);
+  }
+});
+
 
 const createFamily = (req, res) => {
-    const { familyName } = req.body;
-    const family = new Family({
-        name: familyName,
-        key: randomize('A0', 16),
+  const { familyName } = req.body;
+  const family = new Family({
+    name: familyName,
+    key: randomize('A0', 16),
+  });
+
+  family.save()
+    .then(checkSavedFamily)
+    .then(savedFamily => new Promise((resolve, reject) => {
+      User.findById(req.userId, (findErr, user) => {
+        user.family_id = savedFamily.id;
+        user.save((err, updatedUser) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(updatedUser);
+          }
+        });
+      });
+    }))
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(() => {
+      res.redirect('/');
     });
-    family.save().then((savedFamily) => {
-        if(!savedFamily){
-            res.redirect('/joinFamily')
-        } else {
-            User.findById(id, (findErr, user) => {
-                user.family_id = savedFamily.id;
-                user.save(() => {
-                    res.redirect('/');
-                });
-            })
-        }
-    });
-    res.redirect('/');
 };
 
+
+const index = (req, res) => {
+  res.render('joinFamily');
+};
+
+
 module.exports = {
-    createFamily,
+  createFamily,
+  index,
 };
