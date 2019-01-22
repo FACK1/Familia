86d733cc640bb2027266efdfc52683bfcb06fcca
orjwan98 <orjwan98@hotmail.com>
2019-01-22 09:57:26 +0200

    make join handler

--- src/controllers/familyController.js
+++ src/controllers/familyController.js
@@ -2,7 +2,6 @@ const randomize = require('randomatic');
 const Family = require('../models/Family.model');
 const User = require('../models/User.model');
 
-
 const checkSavedFamily = isSavedFamily => new Promise((resolve, reject) => {
   if (!isSavedFamily) {
     reject();
@@ -11,7 +10,6 @@ const checkSavedFamily = isSavedFamily => new Promise((resolve, reject) => {
   }
 });
 
-
 const createFamily = (req, res) => {
   const { familyName } = req.body;
   const family = new Family({
@@ -19,20 +17,23 @@ const createFamily = (req, res) => {
     key: randomize('A0', 16),
   });
 
-  family.save()
+  family
+    .save()
     .then(checkSavedFamily)
-    .then(savedFamily => new Promise((resolve, reject) => {
-      User.findById(req.userId, (findErr, user) => {
-        user.family_id = savedFamily.id;
-        user.save((err, updatedUser) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(updatedUser);
-          }
+    .then(
+      savedFamily => new Promise((resolve, reject) => {
+        User.findById(req.userId, (findErr, user) => {
+          user.family_id = savedFamily.id;
+          user.save((err, updatedUser) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(updatedUser);
+            }
+          });
         });
-      });
-    }))
+      }),
+    )
     .then(() => {
       res.redirect('/');
     })
@@ -41,13 +42,38 @@ const createFamily = (req, res) => {
     });
 };
 
+const findFamily = key => new Promise((resolve, reject) => {
+  Family.findOne({ key }).then((foundFam) => {
+    if (!foundFam) {
+      reject(foundFam);
+    } else {
+      resolve(foundFam);
+    }
+  });
+});
+
+const join = (req, res) => {
+  const { key } = req.body;
+  findFamily(key).then((foundFam) => {
+    User.findById(req.userId, (findErr, foundUser) => {
+      foundUser.family_id = foundFam.id;
+      foundUser.save((err, updatedUser) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.redirect('/');
+        }
+      });
+    });
+  });
+};
 
 const index = (req, res) => {
   res.render('joinFamily');
 };
 
-
 module.exports = {
   createFamily,
   index,
+  join,
 };
